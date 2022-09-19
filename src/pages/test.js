3ad84e2c1e70d727d55dcc6aab718cba07d5ef88
leewooyup <zjadlspun1114@naver.com>
2022-09-19 09:58:34 +0900

    Link로 구성된 GNB 추가

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,9 +1,16 @@
+import { Link } from "gatsby";
 import React from "react";
 
 function TestPage() {
   return (
     <main>
       <title>테스트</title>
+      <div style={{ display: "flex", gap: 20 }}>
+        <Link to="/">홈</Link>
+        <Link to="/about">어바웃</Link>
+        <Link to="/test">테스트</Link>
+        <Link to="/test-space">테스트스페이스</Link>
+      </div>
       <h1>테스트 페이지입니다.</h1>
     </main>
   );
