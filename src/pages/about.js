b0cf7c3d0fe50be687c4bab401ce50014800754d
leewooyup <zjadlspun1114@naver.com>
2022-09-19 10:35:46 +0900

    Head 컴포넌트 도입

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,11 @@
-import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/head";
 
 function AboutPage() {
   return (
     <main>
       <title>자기소개</title>
-      <div style={{ display: "flex", gap: 20 }}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/test-space">테스트스페이스</Link>
-      </div>
+      <Head />
       <h1>자기소개 페이지 입니다.</h1>
     </main>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
-import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/head";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
-      <div style={{ display: "flex", gap: 20 }}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/test-space">테스트스페이스</Link>
-      </div>
+      <Head />
       <h1>Home Page</h1>
     </main>
   );
